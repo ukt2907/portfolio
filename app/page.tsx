d36710fd4256b516d8b2660c65645d9e78c21e12
Umesh Thakur <thakur.sonunet.sonu@gmail.com>
2025-03-21 03:10:03 +0530

    Added Navbar Animation

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import ContactMe from "@/components/ContactMe";
 import HeroSection from "@/components/HeroSection";
-import Navbar from "@/components/Navbar";
+import { Navbar } from "@/components/Navbar";
 import Projects from "@/components/Projects";
 import TechSkills from "@/components/TechSkills";
 import { BriefcaseBusiness, ContactIcon, House, UserRound } from "lucide-react";
@@ -16,7 +16,7 @@ export default function Home() {
   return (
     <main className="bg-[#0A0A0A] px-5 sm:px-10 relative  text-white min-h-screen  w-full">
       <div className=" w-full   mx-auto ">
-        <Navbar navitems={navItems}/>
+        <Navbar navItems={navItems}/>
         <HeroSection/>
         <TechSkills/>
         <Projects/>
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,29 +1,79 @@
-import Link from "next/link"
-import { JSX } from "react"
+"use client";
+import React, { JSX, useState } from "react";
+import {
+  motion,
+  AnimatePresence,
+  useScroll,
+  useMotionValueEvent,
+} from "motion/react";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
 
+export const Navbar = ({
+  navItems,
+  className,
+}: {
+  navItems: {
+    name: string;
+    link: string;
+    icon?: JSX.Element;
+  }[];
+  className?: string;
+}) => {
+  const { scrollYProgress } = useScroll();
 
+  const [visible, setVisible] = useState(true);
 
-const Navbar = ({navitems, classname}: {navitems:{
-    name:string,
-    link: string,
-    icon: JSX.Element
-}[], classname?: string}) => {
+  useMotionValueEvent(scrollYProgress, "change", (current) => {
+    // Check if current is not undefined and is a number
+    if (typeof current === "number") {
+      let direction = current! - scrollYProgress.getPrevious()!;
+
+      if (scrollYProgress.get() < 0.05) {
+        setVisible(true);
+      } else {
+        if (direction < 0) {
+          setVisible(true);
+        } else {
+          setVisible(false);
+        }
+      }
+    }
+  });
+
   return (
-    <>
-     <div className="border z-20 px-8 py-4 absolute top-9 left-1/2 -translate-x-1/2   bg-[#1A1A1A] gap-4  rounded-lg flex justify-center border-zinc-600 ">
-        {navitems.map((navitem, id)=> (
-            <Link 
-             key={`Link=${id}`}
-             href={navitem.link}>
-                <div className="flex ">
-                  <span className="sm:hidden">{navitem.icon}</span>
-                  <span className="hidden sm:block">{navitem.name}</span>
-                </div>
-            </Link>
+    <AnimatePresence mode="wait">
+      <motion.div
+        initial={{
+          opacity: 1,
+          y: -100,
+        }}
+        animate={{
+          y: visible ? 0 : -100,
+          opacity: visible ? 1 : 0,
+        }}
+        transition={{
+          duration: 0.2,
+        }}
+        className={cn(
+          "flex max-w-fit   fixed top-10 inset-x-0 mx-auto border border-transparent dark:border-white/[0.2] rounded-full bg-[#1a1a1a]  shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-[5000] px-8 py-2  items-center justify-center space-x-4",
+          className
+        )}
+      >
+        {navItems.map((navItem: any, idx: number) => (
+          <Link
+            key={`link=${idx}`}
+            href={navItem.link}
+            className={cn(
+              "relative items-center flex space-x-1 text-white"
+            )}
+          >
+            <span className="block sm:hidden">{navItem.icon}</span>
+            <span className="hidden sm:block text-sm">{navItem.name}</span>
+          </Link>
         ))}
-     </div>
-    </>
-  )
-}
 
-export default Navbar
+      </motion.div>
+    </AnimatePresence>
+  );
+};
